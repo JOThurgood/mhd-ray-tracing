59f1124950b3e0ddb138ca58ccf6d832a52b074a
Jonathan Thurgood <jonathan.thurgood@northumbria.ac.uk>
2018-09-14 22:46:57 +0100

    added option to use different solver in new solve (make more graceful later). Also moved abs(b) normalisation on r0 into swarm helper

--- wkb_fast_zerobeta.py
+++ wkb_fast_zerobeta.py
@@ -1,6 +1,6 @@
 import numpy as np
 import math
-from scipy.integrate import odeint
+from scipy.integrate import odeint, solve_ivp
 
 # This next block handles usr choice of magnetic_field clss
 import importlib  
@@ -55,6 +55,33 @@ def df_ds(f, s):
     df = (dt, dx, dy, dz, dp, dq, dr)
     return df 
 
+def newdf_ds(s, f):
+    # Helper variables
+    t = f[0]
+    omega = 2.0 * math.pi  # hardcoded as const
+    phi = 0.0   # could generalise later... 
+    x = f[1]
+    y = f[2]
+    z = f[3]
+    p = f[4]
+    q = f[5]
+    r = f[6] 
+    k2 = p**2 + q**2 + r**2
+    b = magnetic_field(x, y, z) 
+    b2 = b.x**2 + b.y**2 + b.z**2
+    # Derivatives with respect to s
+    dt = -omega #James' code had -2pi**2? 
+    dx = p * b2
+    dy = q * b2
+    dz = r * b2 
+    dp = -k2 * (b.x*b.x_dx + b.y*b.y_dx + b.z*b.z_dx)  
+    dq = -k2 * (b.x*b.x_dy + b.y*b.y_dy + b.z*b.z_dy)  
+    dr = -k2 * (b.x*b.x_dz + b.y*b.y_dz + b.z*b.z_dz)  
+    # dphi = 0.0 # hardcoded as const
+    # domega = 0.0  
+    # Collect result as tuple in appropriate order
+    df = (dt, dx, dy, dz, dp, dq, dr)
+    return df 
 
 class Ray:
 
@@ -86,6 +113,18 @@ class Ray:
         self.p = f[:, 4]
         self.q = f[:, 5]
         self.r = f[:, 6]
+    
+    def newsolve(self, s_end, ns):
+        sln = solve_ivp(newdf_ds,(0,s_end),self.f0,method='Radau')
+        self.f = sln.y
+        self.t = sln.y[0]
+        self.x = sln.y[1]
+        self.y = sln.y[2]
+        self.z = sln.y[3]
+        self.p = sln.y[4]
+        self.q = sln.y[5]
+        self.r = sln.y[6]
+        
 
 class Swarm:  # Essentially a list of Rays plus associated functions
 
@@ -102,8 +141,10 @@ class Swarm:  # Essentially a list of Rays plus associated functions
             r = index[5]
             self.rays.append(Ray(x,y,z,p,q,r))
 
-    # Initialise rays at evenly spaced points along a line with same
-    # p0,q0,r0 
+    # Initialise rays at evenly spaced points along a line with same 
+    # p0,q0.
+    # r0 is uniformly modified by a factor of 1/abs(b) in here
+    # as it is appropriate for my test probel,
     @classmethod
     def init_line_linspace(cls,x0,x1,y0,y1,z0,z1,p,q,r,nrays):
         #use d as 'radius' since r is grad_x k and reserved
@@ -116,8 +157,10 @@ class Swarm:  # Essentially a list of Rays plus associated functions
             x = x0 + dp * math.sin(theta) * math.cos(phi)
             y = y0 + dp * math.sin(theta) * math.sin(phi)
             z = z0 + dp * math.cos(theta)
+            b0 = magnetic_field(x,y,z)
+            r_el = r / b0.abs   
             # print(dp,x,y,z)
-            element = (x,y,z,p,q,r)
+            element = (x,y,z,p,q,r_el)
             coordlist.append(element)
         return cls(coordlist)
 
