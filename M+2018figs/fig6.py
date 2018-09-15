2feb82962b8488739cce27dfabdb8565a3a84170
Jonathan Thurgood <jonathan.thurgood@northumbria.ac.uk>
2018-09-15 21:39:33 +0100

    change sign of r0 for consistency with signfix in solver

--- M+2018figs/fig6.py
+++ M+2018figs/fig6.py
@@ -27,8 +27,7 @@ z0=z1=0
 nrays = 300  
 p = 0
 q = 0
-#b0 = magnetic_field(x0,y0,z0)
-r = 2.0 * math.pi #/ b0.abs
+r = -2.0 * math.pi
 
 swarm1 = wkb.Swarm.init_line_linspace(x0,x1,y0,y1,z0,z1,p,q,r,nrays)
 
--- M+2018figs/fig8_notfinished.py
+++ M+2018figs/fig8_notfinished.py
@@ -30,7 +30,7 @@ ny = 100
 p = 0
 q = 0
 #b0 = magnetic_field(x0,y0,z0)
-r = 2.0 * math.pi #/ b0.abs
+r = - 2.0 * math.pi #/ b0.abs
 
 swarm1 = wkb.Swarm.init_zplane(x0,x1,y0,y1,z,nx,ny,p,q,r)
 
