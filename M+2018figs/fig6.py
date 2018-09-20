d50cab8ee3f550dbdb6d7f87d9e8418565a1a64a
Jonathan Thurgood <jonathanthurgood1989@gmail.com>
2018-09-20 22:09:19 +0100

    minor format changes

--- M+2018figs/fig6.py
+++ M+2018figs/fig6.py
@@ -37,7 +37,8 @@ for iz in range(0,nz-1):
 
 t_end = 4.00 
 s_end = t_end / 2.0 / math.pi
-ns = 40 + 1# sampling number for solution (does not effect dh directly)
+ns = 40 + 1
+ns = 4000 + 1# sampling number for solution (does not effect dh directly)
 
 x0 = 0
 x1 = 0
@@ -54,6 +55,7 @@ swarm1 = wkb.Swarm.init_line_linspace(x0,x1,y0,y1,z0,z1,p,q,r,nrays)
 
 
 swarm1.solve(s_end,ns)
+print('ODE solve for swarm done. Now making the figures at the sampling times')
 
 outdir = 'frames_fig6'
 if not os.path.exists(outdir):
@@ -63,7 +65,8 @@ for i in range(0,ns-1):
     plt.contour(gy,gz,ax,levels=[0],colors='red')
     for myray in swarm1.rays:
         plt.plot(myray.y[i],myray.z[i],'k+')
-    title = '{:0.2f}'.format(swarm1.rays[1].t[i])
+#    title = '{:0.2f}'.format(swarm1.rays[1].t[i])
+    title = '{:0.3f}'.format(swarm1.rays[1].t[i])
     title = 't = '+title
     plt.xlabel('y')
     plt.ylabel('z')
@@ -72,4 +75,6 @@ for i in range(0,ns-1):
     filename = '{:04d}'.format(i)
     filename = outdir+'/'+filename+'.png'
     plt.savefig(filename,dpi=300)
+    msg = 'saved image '+filename    
+    print(msg)
     plt.clf()
