a98f93afe33af4a38496624b5f33e183e651b1d8
Jonathan Thurgood <jonathanthurgood1989@gmail.com>
2018-09-20 22:00:01 +0100

    add separatrix plots to fig6

--- M+2018figs/fig6.py
+++ M+2018figs/fig6.py
@@ -10,9 +10,30 @@ import matplotlib.pyplot as plt
 import wkb_fast_zerobeta as wkb
 import math
 from magnetic_field import Dipole3dCase1 as magnetic_field
+import numpy as np
 # Remove the f
 import os
 os.remove('tmp_config.py')
+
+# Calclate the contours of the flux functon
+y0 = -2.10
+y1 = 2.10
+z0 = 0.
+z1 = 3.10
+ny = 4*25
+nz = ny
+gy = np.linspace(y0,y1,ny)
+gz = np.linspace(z0,z1,nz)
+
+ax = np.zeros([ny,nz])
+speed = np.zeros([ny,nz])
+for iz in range(0,nz-1):
+    for iy in range(0,ny-1):
+        b = magnetic_field(0.0,gy[iy], gz[iz])
+        ax[iz,iy] = b.ax 
+        speed[iz,iy] = b.abs
+
+# solve the rays iteratively
 
 t_end = 4.00 
 s_end = t_end / 2.0 / math.pi
@@ -39,6 +60,7 @@ if not os.path.exists(outdir):
     os.mkdir(outdir)
 
 for i in range(0,ns-1):
+    plt.contour(gy,gz,ax,levels=[0],colors='red')
     for myray in swarm1.rays:
         plt.plot(myray.y[i],myray.z[i],'k+')
     title = '{:0.2f}'.format(swarm1.rays[1].t[i])
